b0e147274f5a907a6f167e3eb708e6b95b324f19
Rowan <heatherngee@gmail.com>
2022-03-31 12:54:01 -0400

    Last window of GUI displaying results

--- BoardingPassProject/src/BoardingPassTicketWindow.java
+++ BoardingPassProject/src/BoardingPassTicketWindow.java
@@ -5,7 +5,8 @@ import java.text.ParseException;
 public class BoardingPassTicketWindow extends DepartureDateTimeWindow {
 
     JFrame frame = new JFrame("Generated Boarding Pass");
-    JLabel boardingPassNumberLabel, flightDetailsLabel, fromToLabel, userInformationLabel, ticketPriceLabel, label;
+    JLabel boardingPassNumberLabel, originLabel, destinationLabel, departureDateLabel, departureTimeLabel, etaLabel,
+            nameLabel, ageLabel, genderLabel, emailLabel, phoneNumberLabel, ticketPriceLabel, label;
     Member member;
 
     BoardingPassTicketWindow(Member member) throws ParseException {
@@ -26,37 +27,83 @@ public class BoardingPassTicketWindow extends DepartureDateTimeWindow {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         boardingPassNumberLabel = new JLabel("Boarding Pass Number: " + member.getBoardingPassNumber());
-        boardingPassNumberLabel.setBounds(355,70,600,30);
+        boardingPassNumberLabel.setBounds(355,70,400,30);
         boardingPassNumberLabel.setFont(new Font(null, Font.BOLD, 15));
         frame.add(boardingPassNumberLabel);
 
-        userInformationLabel = new JLabel("Name of Passenger: " + member.getName() + '\n' +
-                "Age: " + member.getAge() + '\n' + "M/F: " + member.getGender() + '\n' + "Email: " + member.getEmail() +
-                '\n' + "Phone Number: " + member.getPhoneNumber());
-        userInformationLabel.setBounds(355,100,600,30);
-        userInformationLabel.setFont(new Font(null, Font.BOLD, 15));
-        frame.add(userInformationLabel);
+        nameLabel = new JLabel("Name of Passenger: " + member.getName());
+        nameLabel.setBounds(355,110,400,30);
+        nameLabel.setFont(new Font(null, Font.BOLD, 15));
+        frame.add(nameLabel);
 
-        fromToLabel = new JLabel("Leaving from: " + member.getOrigin() + " Will arrive at: " + member.getDestination());
-        fromToLabel.setBounds(355,250,600,30);
-        fromToLabel.setFont(new Font(null, Font.BOLD, 15));
-        frame.add(fromToLabel);
+        ageLabel = new JLabel("Age: " + member.getAge());
+        ageLabel.setBounds(355,150,400,30);
+        ageLabel.setFont(new Font(null, Font.BOLD, 15));
+        frame.add(ageLabel);
 
-        System.out.print(member);
-        flightDetailsLabel = new JLabel("Leaving on: " + member.getDepartureDate() + '\n' +
-                "At: " + member.getDepartureTime() + '\n' +
-                "Will arrive at: " + member.getETA());
-                //Throwing errors
-                /*fix problem w/ line 147 in Member,
-                line 50 + 33 in Ticket,
-                line 46 in BoardingPassTicketWindow,
-                line 95 in DepartureDateTimeWindow(4 internal errors) BasicButtonListener*/
-        flightDetailsLabel.setBounds(355,280,600,30);
-        flightDetailsLabel.setFont(new Font(null, Font.BOLD, 15));
-        frame.add(flightDetailsLabel);
+        genderLabel = new JLabel("Gender: " + member.getGender());
+        genderLabel.setBounds(355,190,400,30);
+        genderLabel.setFont(new Font(null, Font.BOLD, 15));
+        frame.add(genderLabel);
+
+        emailLabel = new JLabel("Email: " + member.getEmail());
+        emailLabel.setBounds(355,230,400,30);
+        emailLabel.setFont(new Font(null, Font.BOLD, 15));
+        frame.add(emailLabel);
+
+        phoneNumberLabel = new JLabel("Phone Number: " + member.getPhoneNumber());
+        phoneNumberLabel.setBounds(355,270,400,30);
+        phoneNumberLabel.setFont(new Font(null, Font.BOLD, 15));
+        frame.add(phoneNumberLabel);
+
+        originLabel = new JLabel("Leaving From: " + member.getOrigin());
+        originLabel.setBounds(355,310,400,30);
+        originLabel.setFont(new Font(null, Font.BOLD, 15));
+        frame.add(originLabel);
+
+        destinationLabel = new JLabel("Will arrive at: " + member.getDestination());
+        destinationLabel.setBounds(355,350,400,30);
+        destinationLabel.setFont(new Font(null, Font.BOLD, 15));
+        frame.add(destinationLabel);
+
+        departureDateLabel = new JLabel("Leaving on: " + member.getDepartureDate());
+        departureDateLabel.setBounds(355,390,400,30);
+        departureDateLabel.setFont(new Font(null, Font.BOLD, 15));
+        frame.add(departureDateLabel);
+
+        departureTimeLabel = new JLabel("At: " + member.getDepartureTime());
+        departureTimeLabel.setBounds(355,430,400,30);
+        departureTimeLabel.setFont(new Font(null, Font.BOLD, 15));
+        frame.add(departureTimeLabel);
+
+        etaLabel = new JLabel("Will Arrive At: " + member.getETA());
+        etaLabel.setBounds(355,470,400,30);
+        etaLabel.setFont(new Font(null, Font.BOLD,15));
+        frame.add(etaLabel);
+
+
+//        userInformationLabel = new JLabel("Name of Passenger: " + member.getName() + '\n' +
+//                "Age: " + member.getAge() + '\n' + "M/F: " + member.getGender() + '\n' + "Email: " + member.getEmail() +
+//                '\n' + "Phone Number: " + member.getPhoneNumber());
+//        userInformationLabel.setBounds(355,100,600,30);
+//        userInformationLabel.setFont(new Font(null, Font.BOLD, 15));
+//        frame.add(userInformationLabel);
+//
+//        fromToLabel = new JLabel("Leaving from: " + member.getOrigin() + " Will arrive at: " + member.getDestination());
+//        fromToLabel.setBounds(355,250,600,30);
+//        fromToLabel.setFont(new Font(null, Font.BOLD, 15));
+//        frame.add(fromToLabel);
+//
+//        System.out.print(member);
+//        flightDetailsLabel = new JLabel("Leaving on: " + member.getDepartureDate() + '\n' +
+//                "At: " + member.getDepartureTime() + '\n' +
+//                "Will arrive at: " + member.getETA());
+//        flightDetailsLabel.setBounds(355,280,600,30);
+//        flightDetailsLabel.setFont(new Font(null, Font.BOLD, 15));
+//        frame.add(flightDetailsLabel);
 
         ticketPriceLabel = new JLabel("Total Ticket Price: " + member.getTicketPrice());
-        ticketPriceLabel.setBounds(355,370,600,30);
+        ticketPriceLabel.setBounds(355,510,600,30);
         ticketPriceLabel.setFont(new Font(null, Font.BOLD, 15));
         frame.add(ticketPriceLabel);
 
